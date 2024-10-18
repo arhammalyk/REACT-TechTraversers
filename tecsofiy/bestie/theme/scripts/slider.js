1675d4446ab9849e1c4d61c78754f50d5c1372ba
arhammalyk <rahammalik402@gmail.com>
2024-10-18 22:36:56 +0500

    Feat:Header & footer

---
+++ tecsofiy/bestie/theme/scripts/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".variable-width").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 3000,
+    cssEase: "linear",
+    infinite: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+  });
+});
